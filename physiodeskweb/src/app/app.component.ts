839164dec901dabc68b2239254d8646890648c9d
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-04-23 23:42:53 -0500

    add: header

--- physiodeskweb/src/app/app.component.ts
+++ physiodeskweb/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainContainerComponent} from "./components/main-container/main-container.component";
+import {HeaderComponent} from "./public/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainContainerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
