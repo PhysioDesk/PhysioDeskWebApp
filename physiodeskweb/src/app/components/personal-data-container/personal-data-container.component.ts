06f5126ebd0e581dd68c1a401a0ccf4d8e5389e9
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-04-28 16:03:08 -0500

    edit: Api Implementacion

--- physiodeskweb/src/app/components/personal-data-container/personal-data-container.component.ts
+++ physiodeskweb/src/app/components/personal-data-container/personal-data-container.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {SidebarComponent} from "../../public/sidebar/sidebar.component";
+import {FisiosTableComponent} from "../../fisioterapeutas/components/fisios-table/fisios-table.component";
 
 @Component({
   selector: 'app-personal-data-container',
   standalone: true,
-  imports: [
-    SidebarComponent
-  ],
+    imports: [
+        SidebarComponent,
+        FisiosTableComponent
+    ],
   templateUrl: './personal-data-container.component.html',
   styleUrl: './personal-data-container.component.css'
 })
--- physiodeskweb/src/environments/environments.ts
+++ physiodeskweb/src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://my-json-server.typicode.com/erickpalomino1923/PhysioDeskData'
 }
