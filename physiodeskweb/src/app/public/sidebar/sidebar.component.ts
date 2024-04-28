ecc158b334cca116b821571bb3fd930f7c93d6a4
erickpalomino1923 <erickpalomino0723@gmail.com>
2024-04-28 16:43:24 -0500

    add: sidebar component

--- physiodeskweb/src/app/public/sidebar/sidebar.component.ts
+++ physiodeskweb/src/app/public/sidebar/sidebar.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavContainer,
+    MatNavList,
+    MatListItem,
+    MatSidenav,
+    MatSidenavModule,
+    MatIcon,
+    MatIconButton
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 }
